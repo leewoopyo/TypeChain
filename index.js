fcd7a62e909992c5554c1f15faec0db92abae399
woopii <51807270+leewoopyo@users.noreply.github.com>
2020-05-19 14:09:35 +0900

    05-19

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 "use strict";
+//const name = "lee", age = 1, gender = "male";
 Object.defineProperty(exports, "__esModule", { value: true });
-const name = "lee", age = 1, gender = "male";
 const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+    // console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
 };
-sayHi(name, age);
+console.log(sayHi("lee", 10, "male"));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
-const name = "lee", age = 1, gender = "male";
+//const name = "lee", age = 1, gender = "male";
 
-const sayHi = (name, age, gender?) =>{
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+const sayHi = (name:string, age:number, gender?:string): String =>{
+   // console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
 }
 
-sayHi(name,age);
+console.log(sayHi("lee",10,"male"));
 
 export {};
